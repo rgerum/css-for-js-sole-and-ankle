2a13ee3152b37d134c062fba06f8074bc4181eba
Richard Gerum <14153051+rgerum@users.noreply.github.com>
2024-02-26 15:22:42 -0500

    fix baseline in shell

--- src/components/ShoeIndex/ShoeIndex.js
+++ src/components/ShoeIndex/ShoeIndex.js
@@ -45,6 +45,7 @@ const ShoeIndex = ({ sortId, setSortId }) => {
 const Wrapper = styled.div`
 display: flex;
 flex-direction: row-reverse;
+  align-items: baseline;
 `;
 
 const LeftColumn = styled.div`
@@ -57,6 +58,7 @@ flex: 1;
 
 const Header = styled.header`
 display: flex;
+  align-items: baseline;
 `;
 
 const Title = styled.h2`
