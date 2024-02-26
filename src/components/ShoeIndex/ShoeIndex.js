f0102e78a113edcdb33858d9c530601145db9ea5
Richard Gerum <14153051+rgerum@users.noreply.github.com>
2024-02-26 12:47:23 -0500

    shell

--- src/components/ShoeIndex/ShoeIndex.js
+++ src/components/ShoeIndex/ShoeIndex.js
@@ -10,6 +10,7 @@ import ShoeSidebar from '../ShoeSidebar';
 import ShoeGrid from '../ShoeGrid';
 
 const ShoeIndex = ({ sortId, setSortId }) => {
+  //<!-- <ShoeGrid />-->
   return (
     <Wrapper>
       <MainColumn>
@@ -25,7 +26,7 @@ const ShoeIndex = ({ sortId, setSortId }) => {
           </Select>
         </Header>
         <Spacer size={34} />
-        <ShoeGrid />
+
       </MainColumn>
       <LeftColumn>
         <Breadcrumbs>
@@ -42,17 +43,27 @@ const ShoeIndex = ({ sortId, setSortId }) => {
   );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+display: flex;
+flex-direction: row-reverse;
+`;
 
-const LeftColumn = styled.div``;
+const LeftColumn = styled.div`
+  width: 416px;
+`;
 
-const MainColumn = styled.div``;
+const MainColumn = styled.div`
+flex: 1;
+`;
 
-const Header = styled.header``;
+const Header = styled.header`
+display: flex;
+`;
 
 const Title = styled.h2`
   font-size: 1.5rem;
   font-weight: ${WEIGHTS.medium};
+  margin-right: auto;
 `;
 
 export default ShoeIndex;
